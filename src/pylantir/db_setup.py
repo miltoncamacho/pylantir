2961f452552e5cd62428d20f45363375c861bdf7
milton.camachocamach <milton.camachocamach@ucalgary.ca>
2025-02-05 17:25:58 -0700

    small fix

--- src/pylantir/db_setup.py
+++ src/pylantir/db_setup.py
@@ -1,7 +1,7 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from src.models import Base
+from .models import Base
 
 def get_engine(db_path="worklist.db", password=None, echo=False):
     """
